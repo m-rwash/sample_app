5c0504b134cc58e6ec31b27509b2957a1666ea7e
Mohamed Rwash <m.rwash93@yahoo.com>
2017-12-10 01:51:11 +0200

    add static pages

--- Gemfile
+++ Gemfile
@@ -41,6 +41,13 @@ group :development, :test do
   gem 'selenium-webdriver'
 end
 
+group :test do
+  gem 'minitest-reporters'
+  gem 'mini_backtrace'
+  gem 'guard-minitest'
+  gem 'guard'
+end
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,18 +2,15 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get home_path
     assert_response :success
-  end
-
-  test "should get help" do
-    get static_pages_help_url
-    assert_response :success
+    assert_select "title", "Home | Twiitero"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
+    assert_select "title", "About | Twiitero"
   end
 
 end
