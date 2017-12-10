b3a86bdfac20a36c3f7765fa41a1df24a755ebae
Mohamed Rwash <m.rwash93@yahoo.com>
2017-12-10 05:36:43 +0200

    styling home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,13 +4,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get home_path
     assert_response :success
-    assert_select "title", "Home | Twiitero"
+    assert_select "title", "Home | Twittero"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | Twiitero"
+    assert_select "title", "About | Twittero"
   end
 
 end
